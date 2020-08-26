1650ae394891b22cb17670997d26520b1ee6662a
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2020-08-26 14:19:17 +0700

    Update setwelcome.js

--- commands/setwelcome.js
+++ commands/setwelcome.js
@@ -1,4 +1,4 @@
 exports.run = async(client, message, args) => {
-  message.reply(`Just Create a channel with the name: welcome-goodbye or goodbye. select one`)
-  message.reply(`https://cdn.discordapp.com/attachments/692254910980423702/747443143539949658/welcome-image.png`)
+  message.reply(`Just Create a channel with the name: welcome-goodbye or welcome. select one`)
+  message.reply(`https://cdn.discordapp.com/attachments/747436198888472617/747436781737213972/welcome-image.png`)
 }
