3c0dde55813d93eeffca06ea5d8794c382141cec
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2020-08-26 13:59:42 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,143 @@
+const Discord = require("discord.js");
+const client = new Discord.Client()
+//UPTIME ROBOT (WEB)
+const { get } = require("snekfetch");
+const http = require("http");
+const express = require("express");
+const app = express();
+app.get("/", (request, response) => {
+  console.log("Pinging");
+  response.sendStatus(200);
+})
+app.listen(process.env.PORT);
+setInterval(() => {
+http.get('http://discord-tutori4l.glitch.me/');
+}, 280000);
+client.on("ready", async () => {
+  console.log(`${client.user.tag} sudah online!`);
+  client.user.setActivity("d!help");
+});
+client.on("message", async message => {
+  if (message.content === "hai") message.reply("Hai trosss");
+  if (message.content === "halo") message.reply("haloo");
+  if (message.content === "oi") message.reply("Assalamualaikum");
+  if (message.content === "alo") message.reply("alo");
+  if (message.content === "hi") message.reply("Hi");
+  if (message.content === "poker?") message.reply("JUDI TOLOL");
+  if (message.content === "konnichiwa") message.reply("konnichiwa");
+  if (message.content === "kontol") message.reply("dont toxic")
+//COMMAND BOT DI SERVER.JS
+const prefix = "d!"
+if(!message.content.startsWith(prefix)) return null;
+let msg = message.content.toLowerCase();
+let args = message.content.slice(prefix.length).trim().split(" ");
+let cmd = args.shift().toLowerCase();
+let command = cmd;
+  
+  
+
+let commandFiles;
+try{
+  commandFiles = require(`./commands/${cmd}.js`)
+} catch (err) {
+
+}
+const db = require("quick.db")
+const now = Date.now()
+if(db.has(`cd_${message.author.id}`)) {
+  const expirationTime = db.get(`cd_${message.author.id}`) + 3000
+  if(now < expirationTime) {
+  const timeLeft = (expirationTime - now) / 1000;
+		return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${cmd}\` command.`);
+  }
+}
+  db.set(`cd_${message.author.id}`, now);
+  setTimeout(() => {
+    db.delete(`cd_${message.author.id}`)
+  },3000)
+try {
+  commandFiles.run(client, message, args)
+} catch (err) {
+  } finally {
+    console.log(`${message.author.tag} menggunakan command ${prefix}${cmd}`)
+  }
+
+});
+
+//wellcome
+const { CanvasSenpai } = require("canvas-senpai")
+const canva = new CanvasSenpai();
+
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome');
+	if (!channel) return;
+
+   let data = await canva.welcome(member, { link: "https://d1eet599aje0m8.cloudfront.net/wp-content/uploads/2020/07/Stills03_JPEG_3.jpg" })
+
+    const attachment = new Discord.MessageAttachment(
+      data,
+      "welcome-image.png"
+    );
+
+    channel.send(
+      `Welcome to the server, ${member.user.username}!`,
+      attachment
+    );   
+   });
+
+//2
+
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome-goodbye');
+	if (!channel) return;
+
+   let data = await canva.welcome(member, { link: "https://d1eet599aje0m8.cloudfront.net/wp-content/uploads/2020/07/Stills03_JPEG_3.jpg" })
+
+    const attachment = new Discord.MessageAttachment(
+      data,
+      "welcome-image.png"
+    );
+
+    channel.send(
+      `Welcome to the server, ${member.user.username}!`,
+      attachment
+    );   
+   });
+
+//Goodbye 
+client.on('guildMemberRemove', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome-goodbye');
+	if (!channel) return;
+
+   let data = await canva.welcome(member, { link: "https://cdn.discordapp.com/attachments/747422146132705310/747434027253760040/unknown.png" })
+
+    const attachment = new Discord.MessageAttachment(
+      data,
+      "welcome-image.png"
+    );
+
+    channel.send(
+     `Goodbye, ${member.user.username}! We Hope You back again :(`,
+      attachment
+    );   
+   });
+
+//2
+
+client.on('guildMemberRemove', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'goodbye');
+	if (!channel) return;
+
+   let data = await canva.welcome(member, { link: "https://cdn.discordapp.com/attachments/747422146132705310/747434027253760040/unknown.png" })
+
+    const attachment = new Discord.MessageAttachment(
+      data,
+      "goodbye-image.png"
+    );
+
+    channel.send(
+      `Goodbye, ${member.user.username}! We Hope You back again :(`,
+      attachment
+    );   
+   });
+client.login(process.env.TOKEN);
