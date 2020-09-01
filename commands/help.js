468d86316ab5fd1b4b06843267d5cc5c73575778
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2020-09-01 21:29:54 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,10 +8,12 @@ exports.run = async (client, message) => {
   .addField(`Economy:dollar:`,`\`transfer\` \`balance\` \`flip\` \`daily\``)
   .addField(`Music:musical_note:`,`\`play\` \`stop\` \`skip\` \`leave\` \`queue\` \`nowplaying\` \`resume\` \`forceskip\` \`playlist\` \`remove\` \`volume\``)
   .addField(`Fun:slight_smile:`,`\`snipe\` \`weather\` \`meme\` \`say\` \`avatar\` \`eval\` \`ping\` \`calculate\``)
-  .addField(`Bot Info:robot:`,`\`stats\` \`uptime\``)
+  .addField(`Bot Info:robot:`,`\`stats\` \`uptime\` \`invite\``)
   .addField(`Administrator:shield:`,`\`kick\` \`warn\` \`clear\``)
   .addField(`Corona`,`\`corona\``)
   .addField(`Action`,`\`punch\` \`hug\``)
   .addField(`Wellcome Goodbye`,`\`setwelcome\` \`setgoodbye\``)
-  message.channel.send(embed)
+  .addField(`invite link `,`[\`Bot Invite\`](https://discord.com/oauth2/authorize?client_id=726093020981625019&scope=bot&permissions=2146958847)`)
+  message.author.send(embed)
+  message.channel.send
 }
