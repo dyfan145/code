a1cb9a73a2c0d6601d50ee4a61a533a7b66053a1
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2021-02-22 12:42:28 +0700

    Delete setwelcome.js

--- commands/setwelcome.js
+++
@@ -1,4 +0,0 @@
-exports.run = async(client, message, args) => {
-  message.reply(`Just Create a channel with the name: welcome-goodbye or welcome. select one`)
-  message.reply(`https://cdn.discordapp.com/attachments/747436198888472617/747436781737213972/welcome-image.png`)
-}
