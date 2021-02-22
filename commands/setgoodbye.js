9b74813c36888505b9b844660065c4c913994b35
dyfan145 <69150154+dyfan145@users.noreply.github.com>
2021-02-22 12:42:05 +0700

    Delete setgoodbye.js

--- commands/setgoodbye.js
+++
@@ -1,4 +0,0 @@
-exports.run = async(client, message, args) => {
-  message.reply(`Just Create a channel with the name: welcome-goodbye or goodbye. select one`)
-  message.reply(`https://cdn.discordapp.com/attachments/692254910980423702/747443143539949658/welcome-image.png`)
-}
